dbe90318c8baf1c73ea9a34a2e4a83b2f4eb897c
Steven Kuhn <37911976+KuhnSteven@users.noreply.github.com>
2020-06-14 15:17:00 -0400

    Formatting & Text detail

--- satisfactory-tool/src/App.js
+++ satisfactory-tool/src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         Satisfactory Tool
       </header>
@@ -13,35 +13,61 @@ function App() {
       <main>
         <section>
           <h1>Calculator</h1>
-          <p>text</p>
+          <p>
+            Based on the game 'Satisfactory' by CoffeeStain Studios, this
+            application can be utilized for calculating total raw resources
+            needed to make a bunch of items, for use either in expansion or
+            meeting project milestones. For a deeper explanation about
+            Satisfactory see the bottom of the page.
+          </p>
 
           <form>
             <h2>New Item to be Crafted - </h2>
-            <label for="craftedItem">Select what you would like to craft: </label>
-              <select id="craftedItems" name="craftedItems" required>
-              <option value="plate">Plate</option>
-              <option value="rod">Rod</option>
-              <option value="screws">Screws</option>
+            <label for='element'>Select an ingot:</label>
+            <select id='element' name='element' required>
+              <option value='iron'>Iron</option>
+              <option value='copper'>Copper</option>
+              <option value='steel'>Steel</option>
             </select>
-            <label for="craftQuantity">How many of the selected item(s) would you like to craft?</label>
-            <input type="number" id="craftQuantity" name="craftQuantity" value="1" required />
 
-              <label for="element">Select an element:</label>
-              <select id="element" name="element" required>
-                <option value="iron">Iron</option>
-                <option value="copper">Copper</option>
-                <option value="steel">Steel</option>
-              </select>
+            <label for='craftedItem'>Select recipe: </label>
+            <select id='craftedItems' name='craftedItems' required>
+              <option value='plate'>Plate</option>
+              <option value='rod'>Rod</option>
+              <option value='screws'>Screws</option>
+            </select>
+            <label for='craftQuantity'>
+              How many of the selected item(s) would you like to craft?
+            </label>
+            <input
+              type='number'
+              id='craftQuantity'
+              name='craftQuantity'
+              value='1'
+              required
+            />
 
-              <input type="reset" class="btn btn-warning" />
-              <input type="submit" id="submit" class="btn btn-success" />
+            <fieldset>
+              <input type='reset' class='btn btn-warning' />
+              <input type='submit' id='submit' class='btn btn-success' />
+            </fieldset>
           </form>
-
         </section>
       </main>
 
       <footer>
         <h2>Footer Nav</h2>
+        <button class='btn btn-info' onclick='Singleton.toggleHiddenDesc()'>
+          Toggle Game Description
+        </button>
+        <section id='hiddenDesc'>
+          <p>
+            Satisfactory is an FPS open-world factory building sim. You play as
+            an engineer on an alien planet tasked to complete ‘Project Assembly’
+            - a massive machine for a mysterious purpose. Conquer nature, build
+            multi-story factories, and automate to satisfaction!
+          </p>
+        </section>
       </footer>
     </div>
   );
